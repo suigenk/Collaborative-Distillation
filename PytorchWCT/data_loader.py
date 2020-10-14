a219fb2cab40f72a1144a7790f92accb83902b3e
Suigen Koide <suigen.k@gmail.com>
2020-10-14 15:24:02 +0900

    Add debug mode. (#4)

--- PytorchWCT/data_loader.py
+++ PytorchWCT/data_loader.py
@@ -18,11 +18,12 @@ def default_loader(path):
     return Image.open(path).convert('RGB')
 
 class Dataset(data.Dataset):
-    def __init__(self, contentPath, stylePath, texturePath, c_size=0, s_size=0, picked_content_mark=".", picked_style_mark=".", synthesis=False):
+    def __init__(self, contentPath, stylePath, texturePath, c_size=0, s_size=0, picked_content_mark=".", picked_style_mark=".", synthesis=False, debug=False):
       super(Dataset,self).__init__()
       self.content_size = c_size
       self.style_size = s_size
       self.synthesis = synthesis
+      self.debug = debug
       if synthesis:
         self.texturePath = texturePath
         self.texture_image_list = [x for x in listdir(texturePath) if is_image_file(x)]
@@ -34,14 +35,11 @@ class Dataset(data.Dataset):
         pairs = [[c, s] for c in content_imgs for s in style_imgs]
         self.content_image_list = list(np.array(pairs)[:, 0])
         self.style_image_list = list(np.array(pairs)[:, 1])
-      
-      # self.normalize = transforms.Normalize(mean=[103.939,116.779,123.68],std=[1, 1, 1])
-      # normalize = transforms.Normalize(mean=[123.68,103.939,116.779],std=[1, 1, 1])
-      # self.prep = transforms.Compose([
-                  # transforms.Scale(fineSize),
-                  # transforms.ToTensor(),
-                  # #transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
-                  # ])
+
+        if self.debug:
+          self.content_image_list = self.content_image_list[:5]
+
+        print("# of images", len(self.content_image_list))
 
     def __getitem__(self, index):
       if not self.synthesis: # style transfer
@@ -55,8 +53,10 @@ class Dataset(data.Dataset):
           styleImg = transforms.Resize(self.style_size)(styleImg)
         contentImg = transforms.ToTensor()(contentImg)
         styleImg = transforms.ToTensor()(styleImg)
-        return contentImg.squeeze(0), styleImg.squeeze(0), \
-               self.content_image_list[index].split(".")[0] + "_" + self.style_image_list[index].split(".")[0] + ".jpg"
+        
+        imname = self.content_image_list[index].split(".")[0] + ".png"
+        return contentImg.squeeze(0), styleImg.squeeze(0), imname
+               
       
       else: # texture synthesis
         textureImgPath = os.path.join(self.texturePath, self.texture_image_list[index])
@@ -71,8 +71,8 @@ class Dataset(data.Dataset):
             neww = int(w * newh / h)
           textureImg = textureImg.resize((neww,newh))
         w, h = textureImg.size
-        contentImg = torch.rand_like(textureImg)
         textureImg = transforms.ToTensor()(textureImg)
+        contentImg = torch.rand_like(textureImg)
         return contentImg.squeeze(0), textureImg.squeeze(0), self.texture_image_list[index].split(".")[0] + ".jpg"
 
     def __len__(self):
